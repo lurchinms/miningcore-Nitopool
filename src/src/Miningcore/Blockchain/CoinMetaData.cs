0cd6a54ec389f8fb365de5d5a129c1f5ad279666
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2025-05-12 05:59:11 -0500

    Fresh Install

---
+++ src/src/Miningcore/Blockchain/CoinMetaData.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+amespace Miningcore.Blockchain
+{
+    public const string BlockHeightPH = "$height$";
+    public const string BlockHashPH = "$hash$";
+    public static class DevDonation
+     {
+         public const decimal Percent = 0.1m;
+ 
+         public static readonly Dictionary<string, string> Addresses = new()
+         {
+         };
+     }
+ 
+     public static class CoinMetaData
+     {
+         public const string BlockHeightPH = "$height$";
+         public const string BlockHashPH = "$hash$";
+     }
+}
